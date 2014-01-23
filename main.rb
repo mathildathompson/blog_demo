f87b9a32a2889f9faaf5eb88177494b607095f2b
Mathilda Thompson <mathildathompson288@gmail.com>
2014-01-23 12:45:28 +1100

    Added in more routes

--- main.rb
+++ main.rb
@@ -1,12 +1,31 @@
 require 'sinatra'
 require 'pry'
+require 'pry-debugger'
 require 'pg'
 require 'active_support/all'
+require 'active_record'
+require 'sinatra/reloader' if development?
+require 'active_record/validations'
+
+
+ActiveRecord::Base.establish_connection(
+    :adapter => 'postgresql',
+    :username => 'mathildathompson',
+    :database => 'blogs'
+)
+
+class Blog < ActiveRecord::Base
+    validates :name, :presence => true
+end
 
 get '/' do
-    sql = "SELECT * FROM blog;"
-    @result = run_sql(sql)
+    @blog = Blog.all
     erb :home
+end
+
+get '/blog/:id' do |id|
+    @blog = Blog.find (id)
+    erb :blog
 end
 
 get '/new' do
@@ -14,26 +33,51 @@ get '/new' do
 end
 
 post '/create' do
-    sql = "INSERT INTO blog (name, comments) values('#{params[:name]}', '#{params[:comments]}');"
-    run_sql(sql)
+    @new_blog = Blog.create(params[:blogs])
+    if @new_blog.valid?
+        redirect to '/'
+    else
+        erb :edit
+    end
+end
+
+get '/blogs/:id/update' do
+    @blog = Blog.find(params[:id])
+    erb :update
+end
+
+post '/blogs/:id/update' do
+    @blog = Blog.find(params[:id])
+    @blog.update_attributes(params[:blogs])
+    redirect to "/blog/#{@blog.id}"
+end
+
+delete '/blogs/:id/delete' do
+    binding.pry
+    @blog = Blog.find(params[:id])
+    @blog.delete
     redirect to '/'
 end
 
-post 'blog/:id/edit' do
-    sql = "UPDATE cats set name='#{params[:name]}', photo='#{params[:photo]}', breed='#{params[:breed]}' WHERE id=#{params[:id]}"
-    run_sql(sql)
-    redirect to '/'
+# get '/edit' do
+#     erb :edit
+# end
 
-end
+# post '/blog/:id/edit' do
+#     sql = "UPDATE cats set name='#{params[:name]}', photo='#{params[:photo]}', breed='#{params[:breed]}' WHERE id=#{params[:id]}"
+#     run_sql(sql)
+#     redirect to '/'
+
+# end
 
 
 
-def run_sql(sql)
-    conn = PG.connect(:dbname => 'Blogosphere')
-    result = conn.exec(sql)
-    conn.close
-    puts result
-    result
-end
+# def run_sql(sql)
+#     conn = PG.connect(:dbname => 'Blogosphere')
+#     result = conn.exec(sql)
+#     conn.close
+#     puts result
+#     result
+# end
 
 # sql = "INSERT INTO CATS (name, photo, breed) values('#{params[:name]}', '#{params[:photo]}', '#{params[:breed]}');"
